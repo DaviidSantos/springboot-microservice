5c3ce8b4a03ac0416612e36b7292578cebd042ec
David Santos <89614359+DaviidSantos@users.noreply.github.com>
2023-02-04 12:32:07 -0300

    Inventory Microservice Implementation

---
+++ inventory-service/src/main/java/com/daviidsantos/inventoryservice/model/Inventory.java
@@ -0,0 +1,20 @@
+package com.daviidsantos.inventoryservice.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
---
+++ inventory-service/src/main/java/com/daviidsantos/inventoryservice/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.daviidsantos.inventoryservice.service;
+
+import com.daviidsantos.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return repository.findBySkuCode(skuCode).isPresent();
+    }
+}
